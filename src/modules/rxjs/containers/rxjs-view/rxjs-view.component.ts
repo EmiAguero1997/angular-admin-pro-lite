6b17b9d3abb1b8831f45fb1d4f92a9adad932d3b
eaguerofolcode <eaguero@folcode.com>
2023-01-17 13:18:22 -0300

    iterable http requests

--- src/modules/rxjs/containers/rxjs-view/rxjs-view.component.ts
+++ src/modules/rxjs/containers/rxjs-view/rxjs-view.component.ts
@@ -1,6 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Observable, Subscription } from 'rxjs';
-import { retry, take, map, filter } from 'rxjs/operators';
+import { forkJoin, interval, Observable, Subscription } from 'rxjs';
+import { retry, take, map, filter, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs-view',
@@ -9,10 +10,21 @@ import { retry, take, map, filter } from 'rxjs/operators';
 })
 export class RxjsViewComponent implements OnInit, OnDestroy {
 
-  intervalSubscription:Subscription
+  intervalSubscription:Subscription;
+  pokemons:any;
+  pokemons2:any = [];
+  subs:Subscription[]=[];
 
-  constructor() {
-    this.intervalSubscription = this.retornIntervalo().subscribe(
+  constructor(private _http:HttpClient) {
+    this.getPokemons().subscribe({
+      next:response=>{
+        this.pokemons = response.results;
+        this.handler().then(()=>{     //SYNCHRONOUS ITERABLE HTTP REQ: handler() returns a promise, we wait the promise to finish and then we write
+          console.log(this.pokemons2);
+        })
+      }
+    });
+    this.intervalSubscription = this.retornaObs().subscribe(
       response=>{
         console.log(response);
       }
@@ -24,6 +36,9 @@ export class RxjsViewComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.intervalSubscription.unsubscribe();
+    this.subs.forEach((element)=>{
+      element.unsubscribe();
+    })
   }
 
   retornaObs():Observable<number>{
@@ -36,10 +51,10 @@ export class RxjsViewComponent implements OnInit, OnDestroy {
           observer.complete()
         )
   
-        i==2 &&(
-          i = -1,
-          observer.error('i llego a 2')
-        )
+        // i==2 &&(
+        //   i = -1,
+        //   observer.error('i llego a 2')
+        // )
       },1000);
     });
     return obs$;
@@ -51,6 +66,38 @@ export class RxjsViewComponent implements OnInit, OnDestroy {
       map(response => response + 1),
       filter(response => response % 2 == 0),
     );
+  }
+
+  getPokemons():Observable<any>{
+    return this._http.get('https://pokeapi.co/api/v2/pokemon');
+  }
+
+  getOnePokemon(name:string):Observable<any>{
+    return this._http.get('https://pokeapi.co/api/v2/pokemon/'+name);
+  }
+
+  handler(){ // this returns a promise wich body has the http requests iteration
+    const promise = new Promise<void>((resolve)=>{
+
+        //iteration
+
+        this.pokemons.forEach((element:any) => {
+
+          //http request
+
+          this.subs.push(this.getOnePokemon(element.name).subscribe({
+            next:response=>{
+              this.pokemons2.push(response);
+            }
+          }))
+          
+        })
+        resolve();
+    })
+
+    //return the promise
+
+    return promise;
   }
 
 }
